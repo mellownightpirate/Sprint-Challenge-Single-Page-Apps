b1f98524dab357cb5c0b6935951f1b790b886951
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-01-31 08:31:56 +0000

    fetched list of characters from API and rendered them to screen with some styles, hooked up the welcome and characters pages with react router

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    Axios.get("https://rickandmortyapi.com/api/character/")
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
+      })
+      .catch(e => console.log(e))
+      .finally(() => {
+        console.log("Axios request finished.");
+      });
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <div>
+        <h2>Characters</h2>
+        <Link className="nav-buttons" to={"/"}>
+          Back
+        </Link>
+      </div>
+      {characters.map(chara => {
+        return <CharacterCard key={chara.id} character={chara} />;
+      })}
     </section>
   );
 }
